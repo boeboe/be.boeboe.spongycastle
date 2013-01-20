50a0702238d9aef40f9630b15b4e91c68e0316f2
Bart Van Bos <bartvanbos@gmail.com>
2013-01-20 16:59:34 +0100

    Added certificate generation examples (chapter 6)

---
+++ src/be/boeboe/spongycastle/chapter6/Utils.java
@@ -0,0 +1,26 @@
+package be.boeboe.spongycastle.chapter6;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
+import java.security.Security;
+import org.spongycastle.jce.provider.BouncyCastleProvider;
+
+/**
+ * Chapter 6 Utils
+ */
+public class Utils extends be.boeboe.spongycastle.chapter3.Utils {
+  static {
+    BouncyCastleProvider prov = new org.spongycastle.jce.provider.BouncyCastleProvider();
+    Security.addProvider(prov);
+  }
+
+  /**
+  * Create a random 1024 bit RSA key pair
+  */
+  public static KeyPair generateRSAKeyPair() throws Exception {
+   KeyPairGenerator kpGen = KeyPairGenerator.getInstance("RSA", "SC");
+   kpGen.initialize(1024, new SecureRandom());
+   return kpGen.generateKeyPair();
+  }
+}
